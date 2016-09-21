3ffcf8038047484746ea09a1cdd39547111daac2
kaylynyuh <yuhkay4@gmail.com>
2016-09-21 16:44:51 -0700

    GET tests successfully testing error messages and status codes

--- test/journal-test.js
+++ test/journal-test.js
@@ -23,6 +23,21 @@ describe('testing journal rotues', function(){
     });
   });
 
+  // describe('testing POST /api/journal', function(){
+  //   it('should return bad for invalid body', function(done){
+  //     request.post('localhost:3000/api/journal')
+  //     .send({headline: 'hello', article: 'good bye'})
+  //     .end((err, res) => {
+  //       if (err) return done(err);
+  //       expect(res.status).to.equal(200);
+  //       expect(res.body.headline).to.equal('hello');
+  //       expect(res.body.article).to.equal('good bye');
+  //       journal = res.body;
+  //       done();
+  //     });
+  //   });
+  // });
+
   describe('testing GET /api/journal', function(){
     it('should return a journal', function(done){
       request.get(`localhost:3000/api/journal?id=${journal.id}`)
@@ -35,4 +50,28 @@ describe('testing journal rotues', function(){
       });
     });
   });
+
+  describe('testing GET /api/journal', function(){
+    it('should return bad request if no id was provided', function(done){
+      request.get('localhost:3000/api/journal')
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        expect(err).to.equal.null;
+        expect(res.text).to.equal('bad request');
+        done();
+      });
+    });
+  });
+
+  describe('testing GET /api/journal', function(){
+    it('should return not found if id is not valid', function(done){
+      request.get('localhost:3000/api/journal?id=678')
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        expect(res.text).to.equal('not found');
+        done();
+      });
+    });
+  });
+
 });
