3d61f6358f8b2e540a7a59c6b8c649e55ffb9c55
kaylynyuh <yuhkay4@gmail.com>
2016-09-21 16:57:18 -0700

    POST request test working

--- test/journal-test.js
+++ test/journal-test.js
@@ -23,20 +23,17 @@ describe('testing journal rotues', function(){
     });
   });
 
-  // describe('testing POST /api/journal', function(){
-  //   it('should return bad for invalid body', function(done){
-  //     request.post('localhost:3000/api/journal')
-  //     .send({headline: 'hello', article: 'good bye'})
-  //     .end((err, res) => {
-  //       if (err) return done(err);
-  //       expect(res.status).to.equal(200);
-  //       expect(res.body.headline).to.equal('hello');
-  //       expect(res.body.article).to.equal('good bye');
-  //       journal = res.body;
-  //       done();
-  //     });
-  //   });
-  // });
+  describe('testing POST /api/journal', function(){
+    it('should return bad request for invalid body', function(done){
+      request.post('localhost:3000/api/journal')
+      .send({})
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        expect(res.text).to.equal('bad request');
+        done();
+      });
+    });
+  });
 
   describe('testing GET /api/journal', function(){
     it('should return a journal', function(done){
@@ -46,6 +43,7 @@ describe('testing journal rotues', function(){
         expect(res.status).to.equal(200);
         expect(res.body.headline).to.equal('hello');
         expect(res.body.article).to.equal('good bye');
+        console.log(res.body);
         done();
       });
     });
