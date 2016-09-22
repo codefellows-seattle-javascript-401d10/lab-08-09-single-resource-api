6c1afeb34b1afaa189d25a1b11c8692ffe656240
raziyeh bazargan <r.bazargan@gmail.com>
2016-09-21 17:18:34 -0700

    GET test : no id

--- lab-raziyeh/test/person-route-test.js
+++ lab-raziyeh/test/person-route-test.js
@@ -8,8 +8,6 @@ require('../server.js');
 describe('testing note rotues', function(){
   var person = null;
 
-// GET - test 400, responds with 'bad request' if no id was provided in the request
-// GET - test 200, response body like {<data>} for a request made with a valid id
 
   describe('testing POST /api/person', function(){
     it('save a Person - POST', function(done){
@@ -31,11 +29,25 @@ describe('testing note rotues', function(){
     it('return an error with unValid ID - GET', function(done){
       request.get('localhost:3000/api/person?id=111')
       .end((err, res) => {
-       // if (err) return done(err);
         expect(res.status).to.equal(404);
         expect(res.text).to.equal('not found');
         done();
       });
     });
   });
+
+  // GET - test 400, responds with 'bad request' if no id was provided in the request
+  describe('testing GET /api/person', function(){
+    it('return an error with no id - GET', function(done){
+      request.get('localhost:3000/api/person')
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        expect(res.text).to.equal('bad request');
+        done();
+      });
+    });
+  });
+  
+  // GET - test 200, response body like {<data>} for a request made with a valid id
+
 });
