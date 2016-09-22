9d38a151d3652116999ccd8e4b0eb61ad78937ad
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-09-22 14:09:18 -0700

    added response and route

---
+++ lab-elizabeth/route/book-route.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const storage = require('../lib/storage.js');
+const response = require('../lib/response.js');
+const Book = require('../model/book.js');
+
+module.exports = function(router){
+  //should return an already existing book
+  router.get('/api/book', function(req, res){
+    if(req.url.query.id){
+      storage.fetchItem('book', req.url.query.id)
+      .then(book => {
+        response.sendJSON(res, 200, book);
+      })
+      .catch(err => {
+        console.error(err);
+        response.sendText(res, 404, 'not found');
+      });
+      return;
+    }
+    response.sendText(res, 400, 'bad request');
+  });
+
+  //should create a book on the storage
+  router.post('/api/book', function(req, res){
+    try {
+      var book = new Book(req.body.title, req.body.author, req.body.description);
+      storage.createItem('book', book);
+      response.sendJSON(res, 200, book);
+    } catch(err) {
+      console.error(err);
+      response.sendText(res, 400, 'bad request');
+    }
+  });
+
+  //should delete an already existing book
+  router.delete('/api/book', function(req, res){
+    if(req.url.query.id){
+      storage.deleteItem('recipe', req.url.query.id)
+      .then(() => {
+        response.sendText(res, 204, 'no content');
+      })
+      .catch(err => {
+        console.error(err);
+        response.sendText(res, 404, 'not found');
+      });
+      return;
+    }
+    response.sendText(res, 400, 'bad request');
+  });
+};
--- lab-elizabeth/server.js
+++ lab-elizabeth/server.js
@@ -4,84 +4,14 @@
 const http = require('http');
 //npm modules
 //app modules
-const Book = require('./model/book.js');
 const Router = require('./lib/router.js');
-const storage = require('./lib/storage.js');
 //module constants
 const PORT = process.env.PORT || 3000;
 const router = new Router();
 //module logic
 
-//should return an already existing recipe
-router.get('/api/book', function(req, res){
-  if(req.url.query.id){
-    storage.fetchItem('book', req.url.query.id)
-    .then(book => {
-      res.writeHead(200, {
-        'Content-Type': 'application/json',
-      });
-      res.write(JSON.stringify(book));
-      res.end();
-    })
-    .catch(err => {
-      console.error(err);
-      res.writeHead(404, {
-        'Content-Type': 'text/plain',
-      });
-      res.writ('not found');
-      res.end();
-    });
-    return;
-  }
-  res.writeHead(400, {
-    'Content-Type': 'text/plain',
-  });
-  res.write('bad request');
-  res.end();
-  // storage.fetchItem()
-});
-
-//should create a recipe on the storage
-router.post('/api/book', function(req, res){
-  try {
-    var book = new Book(req.body.title, req.body.author, req.body.description);
-    storage.createItem('book', book);
-    res.writeHead(200, {
-      'Content-Type': 'application/json',
-    });
-    res.write(JSON.stringify(book));
-    res.end();
-  } catch(err) {
-    console.error(err);
-    res.writeHead(400, {
-      'Content-Type': 'text/plain',
-    });
-    res.write('bad request');
-    res.end();
-  }
-});
-
-//should delete an already existing recipe
-router.delete('/api/book', function(req, res){
-  if(req.url.query.id){
-    storage.deleteItem('recipe', req.url.query.id)
-    .then(() => {
-      res.writeHead(204);
-      res.end();
-    })
-    .catch(err => {
-      console.error(err);
-      res.write('not found');
-      res.end();
-    });
-    return;
-  }
-  res.writeHead(400, {
-    'Content-Type': 'text/plain',
-  });
-  res.write('bad request');
-  res.end();
-});
+// register book routes
+require('./route/book-route.js')(router);
 
 const server = http.createServer(router.route());
 
