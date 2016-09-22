320952a2d6489d3dcd7fcdf25d93a2eed5867970
Lee Broxson <broxsonl@gmail.com>
2016-09-22 09:36:30 -0700

    Filled out the post method on the router on server.js.

--- lab-lee/server.js
+++ lab-lee/server.js
@@ -5,7 +5,7 @@ const http = require('http');
 
 // npm modules
 // app modules
-const Resource = require('./model/resource.js');
+const Note = require('./model/note.js');
 const Router = require('./lib/router.js');
 const storage = require('./lib/storage.js');
 // module constants
@@ -13,3 +13,59 @@ const PORT = process.env.PORT || 3000;
 const router = new Router();
 
 // module logic
+router.get('/api/note', function(req, res) {
+  if (req.url.query.id) {
+    storage.fetchItem('note', req.url.query.id)
+    .then(note => {
+      res.writeHead(200, {
+        'Content-type': 'application/json',
+      });
+      res.write(JSON.stringify(note));
+      res.end();
+    })
+    .catch (err => {
+      console.error(err);
+      res.writeHead(404, {
+        'Content-Type': 'text/plain',
+      });
+      res.write('not found');
+      res.end();
+    });
+    return;
+  }
+  res.writeHead(400, {
+    'Content-Type': 'text/plain',
+  });
+  res.write('bad request');
+  res.end();
+});
+
+router.post('/api/note', function(req, res) {
+  try {
+    var note = new Note(req.body.name, req.body.content);
+    storage.createItem('note', note);
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+    res.write(JSON.stringify(note));
+    res.end();
+  } catch (err) {
+    console.error(err);
+    res.writeHead(400, {
+      'Content-Type': 'text/plain',
+    });
+    res.write('bad request');
+    res.end();
+  }
+});
+
+router.delete('/api/note', function(req, res) {
+  storage.deleteItem(req.url.query.id);
+  res.end();
+});
+
+const server = http.createServer(router.route());
+
+server.listen(PORT, function(){
+  console.log('server is up at', PORT);
+})
