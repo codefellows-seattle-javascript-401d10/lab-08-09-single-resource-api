dd896c397743f7c7e30f47eaa6c953721d12ed60
kaylynyuh <yuhkay4@gmail.com>
2016-09-22 12:39:05 -0700

    refactored journal router

--- route/journal-router.js
+++ route/journal-router.js
@@ -21,47 +21,29 @@ module.exports = function(router) {
   });
 
   router.post('/api/journal', function(req,res) {
-    try {
-      var journal = new Journal(req.body.headline, req.body.article);
-      storage.createItem('journal', journal);
-      res.writeHead(200, {
-        'Content-Type': 'application/json',
-      });
-      res.write(JSON.stringify(journal));
-      res.end();
-    } catch (err) {
+    var journal = new Journal(req.body.headline, req.body.article);
+    storage.createItem('journal', journal)
+    .then( journal => {
+      response.sendJSON(res, 200, journal);
+    })
+    .catch( err => {
       console.error(err);
-      res.writeHead(400, {
-        'Content-Type': 'text/plain',
-      });
-      res.write('bad request');
-      res.end();
-    }
+      response.sendText(res, 404, 'not found');
+    });
   });
 
   router.delete('/api/journal', function(req, res){
     if(req.url.query.id) {
       storage.deleteItem('journal', req.url.query.id)
-      .then (() => {
-        res.writeHead(204, {
-          'Content-Type': 'text/plain'
-        });
-        res.end();
+      .then( journal => {
+        response.sendJSON(res, 200, journal);
       })
-      .catch ( err => {
+      .catch( err => {
         console.error(err);
-        res.writeHead(404, {
-          'Content-Type': 'text/plain',
-        });
-        res.write('not found');
-        res.end();
+        response.sendText(res, 404, 'not found');
       });
       return;
     }
-    res.writeHead(400, {
-      'Content-Type': 'text/plain',
-    });
-    res.write('bad request');
-    res.end();
+    response.sendText(res, 400, 'bad request');
   });
 };
