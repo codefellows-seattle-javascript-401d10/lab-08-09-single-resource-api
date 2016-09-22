fa569dec4e3d0c5c8d52603124c205d394ac77fc
kaylynyuh <yuhkay4@gmail.com>
2016-09-22 16:07:01 -0700

    lab 09 bonus complete

--- lab-kaylyn/lib/storage.js
+++ lab-kaylyn/lib/storage.js
@@ -1,15 +1,17 @@
 'use strict';
 
-const storage = {};
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'), {suffix: 'Prom'});
+const mkdirp = Promise.promisifyAll(require('mkdirp-bluebird'));
 module.exports = exports = {};
 
 exports.createItem = function(schemaName, item){
   if (!schemaName) return Promise.reject(new Error('expected schemaName'));
   if (!item) return Promise.reject(new Error('expected item'));
+
   let json = JSON.stringify(item);
-  return fs.writeFileProm(`${__dirname}/../data/${schemaName}/${item.id}.json`, json)
+  return mkdirp(`${__dirname}/../data/${schemaName}`)
+  .then (fs.writeFileProm(`${__dirname}/../data/${schemaName}/${item.id}.json`, json))
   .then( () => item)
   .catch( err => Promise.reject(err));
 };
