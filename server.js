42f69b81c2336170903a883c7646b3ed9e39a210
kaylynyuh <yuhkay4@gmail.com>
2016-09-22 07:40:43 -0700

    removed commented out code

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 'use strict';
 
-// node modules
 const http = require('http');
-// npm modules
-// app modules
 const Journal = require('./model/journal.js');
 const Router = require('./lib/router.js');
 const storage = require('./lib/storage.js');
-// module constants
 const PORT = process.env.PORT || 3000;
 const router = new Router();
-// module logic
 
 router.get('/api/journal', function(req, res) {
   if (req.url.query.id) {
