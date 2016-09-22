960022098db8c2c5b1f9a0c9609c58255aae46ea
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-09-22 14:26:54 -0700

    updated storage to use bluebird, but npm won't let me install...

--- lab-elizabeth/lib/storage.js
+++ lab-elizabeth/lib/storage.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'), {suffix: 'Prom'});
+
 const storage = {};
 
 module.exports = exports = {};
@@ -8,9 +11,10 @@ exports.createItem = function(schemaName, item){
   if(!schemaName) return Promise.reject(new Error('expected schemaName'));
   if(!item) return Promise.reject(new Error('expected item'));
 
-  if(!storage[schemaName]) storage[schemaName] = {};
-  storage[schemaName][item.id] = item;
-  return Promise.resolve(item);
+  let json = JSON.stringify(item);
+  return fs.writeFileProm(`${__dirname}/../data/${schemaName}/${item.id}.json`, json)
+  .then(() => item)
+  .catch(err => Promise.reject(err));
 };
 
 exports.fetchItem = function(schemaName, id){
@@ -18,11 +22,16 @@ exports.fetchItem = function(schemaName, id){
     if(!schemaName) return reject(new Error('expected schemaName'));
     if(!id) return reject(new Error('expected id'));
 
-    var schema = storage[schemaName];
-    if(!schema) return reject(new Error('schema not found'));
-    var item = schema[id];
-    if(!item) return reject(new Error('item not found'));
-    resolve(item);
+    return fs.readFileProm(`${__dirname}/../data/${schemaName}/${id}.json`)
+    .then(data => {
+      try {
+        let item = JSON.parse(data.toString());
+        return item;
+      } catch(err) {
+        return Promise.reject(err);
+      }
+    })
+    .catch(err => Promise.reject(err));
   });
 };
 
