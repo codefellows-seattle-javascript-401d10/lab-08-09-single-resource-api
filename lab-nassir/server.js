bdd951e33eeae8de3911a649922c2fd112e88273
Nassir Isaf <njisaf@gmail.com>
2016-09-22 13:20:51 -0700

    Refactored to lecture notes

--- lab-nassir/server.js
+++ lab-nassir/server.js
@@ -4,83 +4,15 @@
 const http = require('http');
 //npm modules
 //app modules
-const Cat = require('./model/cat');
 const Router = require('./lib/router');
-const storage = require('./lib/storage');
 //module constants
 const router = new Router();
 const PORT = process.env.PORT || 3000;
+//register cat routes
+require('./route/cat-route')(router);
 //module logic
 
-router.get('/api/cat', function(req, res){
-  if (req.url.query.id) {
-    storage.fetchItem('cat', req.url.query.id)
-    .then( cat => {
-      res.writeHead(200, {
-        'Content-Type': 'application/json',
-      });
-      res.write(JSON.stringify(cat));
-      res.end();
-    })
-    .catch( err => {
-      console.error(err);
-      res.writeHead(404, {
-        'Content-Type': 'text/plain',
-      });
-      res.write('Id not found!');
-      res.end();
-    });
-    return;
-  }
-  res.writeHead(400, {
-    'Content-Type': 'text/plain',
-  });
-  res.write('Bad request!');
-  res.end();
-});
-
-router.post('/api/cat', function(req, res){
-  try {
-    var cat = new Cat(req.body.name, req.body.breed);
-    storage.createItem('cat', cat);
-    res.writeHead(200, {
-      'Content-Type': 'application/json',
-    });
-    res.write(JSON.stringify(cat));
-    res.end();
-  } catch (err) {
-    console.error(err);
-    res.writeHead(400, {
-      'Content-Type': 'text/plain',
-    });
-    res.write('Bad request!');
-    res.end();
-  }
-});
-
-router.delete('/api/cat', function(req, res){
-  if (req.url.query.id) { //what if the if statement is false? we need some more code after this block, a 400 message.
-    storage.deleteItem('cat', req.url.query.id)
-    .then( () => {
-      res.writeHead(204, {
-        'Content-Type': 'text/plain',
-      });
-      res.write('This message will not write.');
-      res.end();
-    })
-    .catch( err => {
-      console.error(err);
-      res.writeHead(400, {
-        'Content-Type': 'text/plain',
-      });
-      res.write('Not found! Need id');
-      res.end(); //put the extra cred here.
-    });
-  }
-});
-
-
-const server = http.createServer(router.route());
+const server = http.createServer(router.route()); 
 
 server.listen(PORT, function(){
   console.log('Welcome to CatAPI, the world\'s least functional cat-based API!\nRemember the CatAPI motto: We exist and owe you no explanation why!(tm)\nThe server is working. Meow.\nListening on PORT', PORT);
