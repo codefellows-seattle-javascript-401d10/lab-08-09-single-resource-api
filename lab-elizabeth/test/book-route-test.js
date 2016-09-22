c653d88ebaa80386dbfe0ae688459138fc143076
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-09-21 21:58:40 -0700

    omg, the tests dont work, I need to read more docs

--- lab-elizabeth/test/book-route-test.js
+++ lab-elizabeth/test/book-route-test.js
@@ -6,68 +6,73 @@ const expect = require('chai').expect;
 require('../server.js');
 
 describe('testing book routes', function(){
-  // var book = null;
+  var book = null;
 
   describe('testing POST /api/book', function(){
-    it('should return a book', function(done){
-      request.post('localhost:3000/api/book.js')
+    it('should create a book', function(done){
+      request.post('localhost:3000/api/book')
       .send({
         author: 'C.S.Lewis',
-        title: 'The Magician\'s Nephew',
+        title: 'The Magicians Nephew',
         description: 'Something about Narnia.',
       })
       .end((err, res) => {
         if(err) return done(err);
         expect(res.status).to.equal(200);
         expect(res.body.author).to.equal('C.S.Lewis');
-        expect(res.body.title).to.equal('the Magician\'s Nephew');
+        expect(res.body.title).to.equal('the Magicians Nephew');
         expect(res.body.description).to.equal('Something about Narnia');
-        // book = res.body;
+        book = res.body;
         done();
       });
     });
-    // it('should report a missing author', function(done){
-    //   request.post('localhost:3000/api/book.js')
-    //   .send({
-    //     author: null,
-    //     title: 'The Magician\'s Nephew',
-    //     description: 'Something about Narnia.',
-    //   })
-    //   .end((err, res) => {
-    //     if(err) return done(err);
-    //     expect(res.status).to.equal(400);
-    //     expect(res.write).to.equal('bad request');
-    //     done();
-    //   });
-    // });
-    // it('should report a missing title', function(done){
-    //   request.post('localhost:3000/api/book.js')
-    //   .send({
-    //     author: 'C.S.Lewis',
-    //     title: null,
-    //     description: 'Something about Narnia.',
-    //   })
-    //   .end((err, res) => {
-    //     if(err) return done(err);
-    //     expect(res.status).to.equal(400);
-    //     expect(res.write).to.equal('bad request');
-    //     done();
-    //   });
-    // });
-    // it('should report a missing description', function(done){
-    //   request.post('localhost:3000/api/book.js')
-    //   .send({
-    //     author: 'C.S.Lewis',
-    //     title: 'The Magician\'s Nephew',
-    //     description: null,
-    //   })
-    //   .end((err, res) => {
-    //     if(err) return done(err);
-    //     expect(res.status).to.equal(400);
-    //     expect(res.write).to.equal('bad request');
-    //     done();
-    //   });
-    // });
+
+    it('should return status 400: bad request', function(done){
+      request.post('localhost:3000/api/book')
+      .send({})
+      .end((err, res) => {
+        if(err) return done(err);
+        expect(res.status).to.equal(400);
+        expect(res.text).to.equal('bad request');
+        done();
+      });
+    });
+  });
+
+  describe('testing GET /api/book', function(){
+    it('should return a book', function(done){
+      request.get(`localhost:3000/api/book?id=${book.id}`)
+      .end((err, res) => {
+        if(err) return done(err);
+        expect(res.status).to.equal(200);
+        expect(res.body.author).to.equal('C.S.Lewis');
+        expect(res.body.title).to.equal('the Magicians Nephew');
+        expect(res.body.description).to.equal('Something about Narnia');
+        done();
+      });
+    });
+
+    it('should return status 404: not found', function(done){
+      request.get('localhost:3000/api/book?id=hippocampus')
+      .send({})
+      .end((err, res) => {
+        if(err) return done(err);
+        expect(res.status).to.equal(404);
+        expect(res.text).to.equal('not found');
+        done();
+      });
+    });
+
+    it('should return status 400: bad request', function(done){
+      request.get('localhost:3000/api/book')
+      .send({})
+      .end((err, res) => {
+        if(err) return done(err);
+        expect(res.status).to.equal(400);
+        expect(res.text).to.equal('bad request');
+        done();
+      });
+    });
   });
 
 });
