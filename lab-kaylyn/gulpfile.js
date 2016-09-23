2a30779df653cd896a88507ea3600ba8a37d946c
kaylynyuh <yuhkay4@gmail.com>
2016-09-23 07:37:28 -0700

    tests passing

--- lab-kaylyn/gulpfile.js
+++ lab-kaylyn/gulpfile.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 const gulp = require('gulp');
@@ -5,19 +6,19 @@ const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
 
 gulp.task('test', function(){
-  gulp.src('./test/*.js', {read:true})
+  gulp.src('./test/*-test.js', {read:true})
   .pipe(mocha({reporter:'nyan'}));
 });
 
 gulp.task('eslint', function(){
-  return gulp.src(['**/*.js','!node_modules/**'])  //everything with.js but not in node_modules folder
+  return gulp.src(['**/*.js','!node_modules/**'])
   .pipe(eslint())
   .pipe(eslint.format())
   .pipe(eslint.failAfterError());
 });
 
 gulp.task('watch', function(){
-  gulp.watch(['**/*.js','./test/*.js'],['eslint','test']);
+  gulp.watch(['./*.js','**/*.js','./lib/*.js'],['eslint','test']);
 });
 
-gulp.task('default',['watch']);
+gulp.task('default',['watch']); 
